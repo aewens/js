0ebb3700ebb9f07918cc52fb1656c23afecd892c
kylobite <kylolink@gmail.com>
2014-04-18 00:39:31 -0500

    ng-learn :: Factories

--- ng-learn/app/scripts/app.js
+++ ng-learn/app/scripts/app.js
@@ -1,9 +1,21 @@
 "use strict";
 
-angular
-    .module("nglearnApp", [
-        "ngCookies",
-        "ngResource",
-        "ngSanitize",
-        "ngRoute"
-    ]);
+var app = angular.module("nglearnApp", ["ngCookies","ngResource","ngSanitize","ngRoute"]);
+
+function TestCtrl($scope) {
+    $scope.data = { message: "World" }
+}
+
+function ThisCtrl($scope) {}
+function ThatCtrl($scope) {}
+
+app.factory("Share", function() {
+    return { message: "Sharing is caring" }
+});
+
+function RedCtrl($scope, Share) {
+    $scope.share = Share;
+}
+function BlueCtrl($scope, Share) {
+    $scope.share = Share;
+}
--- ng-learn/app/scripts/ng.js
+++
@@ -1,5 +0,0 @@
-function TestCtrl($scope) {
-    $scope.data = { message: "World" }
-}
-function ThisCtrl($scope) {}
-function ThatCtrl($scope) {}
